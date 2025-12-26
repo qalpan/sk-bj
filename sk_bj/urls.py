6d6d3f290a9c72c36ac8e9511a4bc74b05c42486
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 20:50:45 +0500

    Update urls.py

--- sk_bj/urls.py
+++ sk_bj/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-# 'core' орнына 'sk_bj' деп жазамыз, себебі views.py осы папкада
-from sk_bj import views  
+from sk_bj import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.resident_auth, name='resident_auth'),
-    path('import-json/', views.import_from_json, name='import_json'),
+    path('import-json/', views.import_from_json, name='import_json'), # Локальді файлды жүктеу беті
     path('signup-special-access/', views.signup, name='signup'),
-    path('import-data/', views.import_data, name='import_data'),
-    path('upload-bank/', views.upload_bank, name='upload_bank'),
+    path('upload-bank/', views.upload_bank_file, name='upload_bank'), # Функция атын views-қа сәйкестендірдік
 ]
