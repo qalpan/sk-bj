5d30c1e1696012fa1149346ad4511550816e6f6c
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 16:20:16 +0500

    Update views.py

--- sk_bj/views.py
+++ sk_bj/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.form.is_valid():
-            user = form.save(commit=False)
-            user.is_staff = True  # Админкаға кіруге рұқсат беру
+        if form.is_valid():
+            user = form.save()
+            # Өзіңізге админ құқығын беру үшін:
+            user.is_staff = True
             user.is_superuser = True
             user.save()
             login(request, user)
