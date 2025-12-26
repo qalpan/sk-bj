039c1132e1616662cc18d8bb62a08ce8269cccbe
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-27 00:55:07 +0500

    Update urls.py

--- sk_bj/urls.py
+++ sk_bj/urls.py
@@ -1,8 +1,18 @@
+from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Бұрынғы бар жолдар (өшірмеңіз, қала берсін)
+    path('login/', views.resident_auth, name='resident_auth'),
+    path('import-json/', views.import_json, name='import_json'),
+    path('signup-special-access/', views.signup, name='signup'),
+    path('upload-bank/', views.upload_bank, name='upload_bank'),
+
+    # БІЗДІҢ ЖАҢА ЫҚШАМДАЛҒАН ЖОЛДАР:
     # Әкімші панелі
     path('admin-panel/', TemplateView.as_view(template_name="tөlemesep_smart.html"), name='admin_panel'),
     
