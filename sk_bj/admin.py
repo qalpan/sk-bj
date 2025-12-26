8c7ae7436a221b6019e8dc7503b19569e85b028d
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 16:44:16 +0500

    Update admin.py

--- sk_bj/admin.py
+++ sk_bj/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Property, Tariff
+from .models import Tariff, Property, Invoice
 
-admin.site.register(Property)
-admin.site.register(Tariff)
+@admin.register(Tariff)
+class TariffAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rate', 'is_per_sqm')
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('address', 'owner_name', 'area')
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('property', 'month', 'total_amount')
