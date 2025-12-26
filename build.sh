a9be1eca2df36c7dc8a265e6987a813b355783f9
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 17:18:22 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,12 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# Нұсқаулықтарды жасау және базаға енгізу
+python manage.py makemigrations sk_bj
 python manage.py migrate
+
+# Егер create_admin.py файлыңыз болса, соны іске қосу
+if [ -f create_admin.py ]; then
+    python create_admin.py
+fi
