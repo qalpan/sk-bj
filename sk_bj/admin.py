089196475b9b8cebd836734c1427c113b723a410
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 18:25:13 +0500

    Update admin.py

--- sk_bj/admin.py
+++ sk_bj/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Property, BankPayment
 
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('address', 'account_number', 'area', 'debt_maint', 'debt_cap')
+    search_fields = ('address', 'account_number')
+    list_filter = ('area',)
+
 @admin.register(BankPayment)
 class BankPaymentAdmin(admin.ModelAdmin):
     list_display = ('property', 'amount', 'payer_name', 'date')
-    list_filter = ('date', 'property')
-    # Бұл жерде сіз CSV-ді жүктейтін сілтеме қоса аласыз
+    date_hierarchy = 'date'
