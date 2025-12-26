5d3a8c7e6ab25678b64b8b062adafe931fa00bf9
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 16:24:11 +0500

    Update settings.py

--- sk_bj/settings.py
+++ sk_bj/settings.py
@@ -41,7 +41,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
