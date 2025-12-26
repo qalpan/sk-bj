2ed611165fe1b1de4c307774ff46399b67f2ea92
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 15:56:35 +0500

    Update urls.py

--- sk_bj/urls.py
+++ sk_bj/urls.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import get_user_model
+
+def create_admin():
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'pass1234')
+
+# Сайт қосылғанда админ жасайды
+try:
+    create_admin()
+except:
+    pass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
