b4de137cd022340d3dbb4cd982073939bb204c3b
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 20:14:39 +0500

    Update urls.py

--- sk_bj/urls.py
+++ sk_bj/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup-special-access/', views.signup, name='signup'),
     path('import-data/', views.import_json_data, name='import_data'), # ОСЫ ЖОЛ
     path('upload-bank/', views.upload_bank_file, name='upload_bank'),
+    path('login/', views.resident_auth, name='resident_auth'),
 ]
