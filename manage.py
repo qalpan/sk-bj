3fe1f51f583138e4bc3c7cb7518004e70e3172d5
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 15:20:19 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,13 +6,20 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sk_bj.settings')
     try:
         from django.core.management import execute_from_command_line
+        # Админ жасау блогы осы жерде болуы керек
+        from django.db import connection
+        from django.contrib.auth.models import User
+        
+        # База дайын болғанда ғана админ жасаймыз
+        execute_from_command_line(['manage.py', 'migrate']) # Базаны алдын ала дайындау
+        
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'pass1234')
+            print("Админ сәтті жасалды: Логин - admin, Пароль - pass1234")
+            
     except ImportError as exc:
         raise ImportError("Django табылмады!") from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'pass1234')
