d700ea41c38139f4791ecced4e6854ba7eee3cbe
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-27 01:00:42 +0500

    Update admin.py

--- sk_bj/admin.py
+++ sk_bj/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from .models import Property, BankPayment
-
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('apartment_id', 'account_number', 'address', 'area') # Мұндағы аттар модельдегімен бірдей болуы тиіс
-    search_fields = ('apartment_id', 'account_number')
-
-@admin.register(BankPayment)
-class BankPaymentAdmin(admin.ModelAdmin):
-    list_display = ('property', 'amount', 'payer_name', 'payment_date')
