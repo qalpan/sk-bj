03323390e2b5023d5fed6b84f50999c0de82be63
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 18:29:43 +0500

    Update urls.py

--- sk_bj/urls.py
+++ sk_bj/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from sk_bj import views  # 'sk_bj' дегенді қосуды ұмытпаңыз
+from sk_bj import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup-special-access/', views.signup, name='signup'),
+    path('import-data/', views.import_json_data, name='import_data'), # ЖАҢА
 ]
