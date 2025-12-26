69a50f78d443c50ce4f91000c1bf15d161c0fe7f
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 16:13:14 +0500

    Update apps.py

--- sk_bj/apps.py
+++ sk_bj/apps.py
@@ -3,15 +3,14 @@ from django.db.models.signals import post_migrate
 
 def create_admin(sender, **kwargs):
     from django.contrib.auth.models import User
-    # Егер 'admin' логині жоқ болса, оны жасайды
+    # Базада 'admin' бар-жоғын тексереді, жоқ болса жасайды
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser('admin', 'admin@example.com', 'pass1234')
-        print("Админ сәтті жасалды!")
 
 class SkBjConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sk_bj'
 
     def ready(self):
-        # post_migrate сигналы база дайын болғанда іске қосылады
+        # post_migrate — база дайын болғанда ғана іске қосылатын сигнал
         post_migrate.connect(create_admin, sender=self)
