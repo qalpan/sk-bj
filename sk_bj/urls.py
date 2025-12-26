de0f294523a2ecef2090d246c58b66a40abe22f7
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-27 01:34:55 +0500

    Update urls.py

--- sk_bj/urls.py
+++ sk_bj/urls.py
@@ -1,13 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    # Тек жұмыс істейтін жаңа жолдарды қалдырамыз
-    path('admin-panel/', TemplateView.as_view(template_name="tөlemesep_smart.html"), name='admin_panel'),
     path('api/data/', views.api_manager, name='api_data'),
-    path('pater/<str:apt_id>/', views.api_manager, name='pater_detail'),
+    path('pater/<str:apt_id>/', views.api_manager, name='pater_page'),
+    path('admin-panel/', views.admin_panel, name='admin_panel'),
 ]
