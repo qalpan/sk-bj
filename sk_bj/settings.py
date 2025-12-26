e5fd39811ca82b04660c0986d242a90cd23100c0
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 17:19:05 +0500

    Update settings.py

--- sk_bj/settings.py
+++ sk_bj/settings.py
@@ -13,7 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sk_bj', # Осы жерге өз папкаңызды қосасыз
+    'sk_bj.apps.SkBjConfig', # Осылай жазыңыз
 ]
 
 MIDDLEWARE = [
