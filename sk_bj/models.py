fd0518bd39b29f414fb0e14ef3632c629eb24760
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 20:04:41 +0500

    Update models.py

--- sk_bj/models.py
+++ sk_bj/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 
 class Property(models.Model):
-    # Пәтер нөмірі (мысалы, "9" немесе "9а") әрқашан бірегей (unique) болуы тиіс
+    # Пәтер нөмірі (мысалы, "9" немесе "9а")
     apartment_id = models.CharField(max_length=50, unique=True) 
     
-    # Дербес шот енді қайталана береді немесе бос бола алады
+    # Дербес шот (Каспи/Халық файлдарымен байланыстыру үшін)
     account_number = models.CharField(max_length=50, blank=True, null=True) 
+    
+    # --- ЖАҢА ӨРІС: Тұрғынның жеке кабинетіне кіру құпия сөзі ---
+    # Әдепкі бойынша "12345" деп орнатамыз, кейін әркім өзі өзгерте алады
+    password = models.CharField(max_length=100, default="12345")
     
     address = models.CharField(max_length=255, default="Мекенжай көрсетілмеген")
     area = models.FloatField(default=0.0)
+    
+    # Қарыздар бөлімі
     debt_maint = models.FloatField(default=0.0)
     debt_clean = models.FloatField(default=0.0)
     debt_sec = models.FloatField(default=0.0)
     debt_heat = models.FloatField(default=0.0)
     debt_cap = models.FloatField(default=0.0)
+
+    # Жалпы қарызды есептейтін функция (Жеке кабинетте көрсету үшін ыңғайлы)
+    def total_debt(self):
+        return round(self.debt_maint + self.debt_clean + self.debt_sec + self.debt_heat + self.debt_cap, 2)
 
     def __str__(self):
         return f"Пәтер {self.apartment_id} (Шот: {self.account_number or 'Жоқ'})"
