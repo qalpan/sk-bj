584e07ceee19ad42d5e695ac823b37b18b9f4cdd
көрме / korme / exhibition / エキシビション / معرض <61244717+qalpan@users.noreply.github.com>
2025-12-26 15:50:38 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
-# Қате шықса тоқтату
 set -o errexit
 
-# Кітапханаларды орнату
 pip install -r requirements.txt
-
-# Статикалық файлдарды жинау
 python manage.py collectstatic --no-input
-
-# Базаны жаңарту (Migrate)
 python manage.py migrate
 
-# Админді Python Shell арқылы жасау
+# Админ жасауды қауіпсіз түрде іске қосу
 python manage.py shell << END
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'pass1234')
-    print('Админ сәтті жасалды!')
-else:
-    print('Админ бұрыннан бар.')
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'pass1234')
+        print('Админ жасалды!')
+    else:
+        print('Админ бұрыннан бар.')
+except Exception as e:
+    print(f'Қате: {e}')
 END
